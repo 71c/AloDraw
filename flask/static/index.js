391fd91cdaf1406bf4d531e459140f4731a35fbb
Alon David Jacobson <alojaco1@gmail.com>
2018-08-05 16:48:30 +0000

    still trying to get zoom good

--- flask/static/index.js
+++ flask/static/index.js
@@ -1,5 +1,53 @@
+var resize = function( img, scale ) {
+    // Takes an image and a scaling factor and returns the scaled image
+
+    // The original image is drawn into an offscreen canvas of the same size
+    // and copied, pixel by pixel into another offscreen canvas with the
+    // new size.
+
+    var widthScaled = img.width * scale;
+    var heightScaled = img.height * scale;
+
+    var orig = document.createElement('canvas');
+    orig.width = img.width;
+    orig.height = img.height;
+    var origCtx = orig.getContext('2d');
+    origCtx.drawImage(img, 0, 0);
+    var origPixels = origCtx.getImageData(0, 0, img.width, img.height);
+
+    var scaled = document.createElement('canvas');
+    scaled.width = widthScaled;
+    scaled.height = heightScaled;
+    var scaledCtx = scaled.getContext('2d');
+    var scaledPixels = scaledCtx.getImageData( 0, 0, widthScaled, heightScaled );
+
+    for( var y = 0; y < heightScaled; y++ ) {
+        for( var x = 0; x < widthScaled; x++ ) {
+            var index = (Math.floor(y / scale) * img.width + Math.floor(x / scale)) * 4;
+            var indexScaled = (y * widthScaled + x) * 4;
+            scaledPixels.data[ indexScaled ] = origPixels.data[ index ];
+            scaledPixels.data[ indexScaled+1 ] = origPixels.data[ index+1 ];
+            scaledPixels.data[ indexScaled+2 ] = origPixels.data[ index+2 ];
+            scaledPixels.data[ indexScaled+3 ] = origPixels.data[ index+3 ];
+        }
+    }
+    scaledCtx.putImageData( scaledPixels, 0, 0 );
+    return scaled;
+};
+
+
+
 var socket;
-var zoomLevel = 4;
+var zoomLevel = 300;
+
+var canvas = document.createElement('canvas');
+canvas.imageSmoothingEnabled = false;
+
+
+
+
+var image = new Image;
+
 document.addEventListener('DOMContentLoaded', () => {
   socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
 
@@ -9,108 +57,87 @@ document.addEventListener('DOMContentLoaded', () => {
   document.querySelector('#zoom-out').setAttribute('onclick', "zoomOut()");
 
   socket.emit('request image');
-  var image;
   socket.on('send image', data => {
-    // var canvas = document.getElementById('canvas');
-    // canvas.setAttribute('width', data.width);
-    // canvas.setAttribute('height', data.height);
-
-    // ctx = canvas.getContext('2d');
-
-
-
-    // var el = document.createElement('img');
-    // // el.src = 'data:image/jpeg;base64,' + data.buffer.toString('base64');
-    // // document.body.append(el);
-    // // ctx.drawImage(el, 0, 0);
-
-
-    // var b64encoded = btoa(String.fromCharCode.apply(null, data.buffer));
-    // var datajpg = "data:image/jpg;base64," + b64encoded;
-    // el.src = datajpg;
-
-    // document.body.append(el);
-
-
-
-
-
-    // var blob = new Blob(data.buffer, {type: 'image/png'});
-    // var url = URL.createObjectURL(blob);
-    // var img = new Image;
-    // img.onload = function() {
-    //     var ctx = document.getElementById("canvas").getContext('2d');
-    //     ctx.drawImage(this, 0, 0);
-    //     URL.revokeObjectURL(url);
-    // };
-    // img.src = url;
-
-
-
-    // console.log(data.buffer);
-    // image = new Image();
-    // // image.src = 'data:image/jpeg;base64,' + data.buffer;
-
-    // var imageData = ctx.createImageData(300, 300);
-    // imageData.data.set(data.buffer);
-
-    // // console.log(imageData.data);
-    // image.src = imageData;
-    // ctx.drawImage(image, 0, 0);
-
-    // ctx.putImageData(data.buffer.Uint8Array, 300, 300);
-    // ctx.createImageData(new ImageData(data.buffer.Uint8Array, [300, 300]));
-
-    // ctx.drawImage(image, 0, 0);
-
-
-
-
-
-
-    image = new Uint8Array(data.buffer);
-    console.log(image);
-
-
-
     var width = data.width;
     var height = data.height;
 
-    var table = document.createElement('table');
+    var buffer = new Uint8ClampedArray(data.buffer);
 
 
-    let i = 0;
-    for (let row = 0; row < height; row++) {
-      let tableRow = document.createElement('tr');
-      for (let col = 0; col < width; col++) {
-        var pixel = document.createElement('div');
+    var ctx = canvas.getContext('2d');
 
-        var red = image[i];
-        i++;
-        var green = image[i];
-        i++;
-        var blue = image[i];
-        i++;
-        i++;
+    // ctx.webkitImageSmoothingEnabled=false;
+    // ctx.mozImageSmoothingEnabled = false;
 
-        pixel.setAttribute('style', 'background-color:rgb(' + red + ',' + green + ',' + blue + ');width:4px;height:4px;');
-        pixel.setAttribute('x', col);
-        pixel.setAttribute('y', row);
+    canvas.width = width;
+    canvas.height = height;
 
-        pixel.setAttribute('onclick', 'changeColor(this);');
+    var idata = ctx.createImageData(width, height);
+    idata.data.set(buffer);
+    ctx.putImageData(idata, 0, 0);
 
-        let tableCol = document.createElement('td');
+    image.src = canvas.toDataURL();
+    // document.body.appendChild(image);
 
-        tableCol.append(pixel);
+    // image = resize(image, 3);
 
 
-        tableRow.append(tableCol);
+
+    ctx.drawImage(image,0,0);
+    document.body.append(canvas);
+
+    document.body.append(image);
+
+    // var dataUri = canvas.toDataURL();
+    // image.src = dataUri;
+
+    // document.body.append(canvas);
 
 
-      }
-      table.append(tableRow);
-    }
-    document.body.append(table);
+
+
+
+
+
+    // var did = document.createElement('div');
+    // did.setAttribute('class', 'flex-container');
+
+    // var table = document.createElement('table');
+
+    // let i = 0;
+    // for (let row = 0; row < height; row++) {
+    //   let tableRow = document.createElement('tr');
+    //   for (let col = 0; col < width; col++) {
+    //     var pixel = document.createElement('div');
+
+    //     var red = buffer[i];
+    //     i++;
+    //     var green = buffer[i];
+    //     i++;
+    //     var blue = buffer[i];
+    //     i++;
+    //     i++;
+
+    //     pixel.setAttribute('style', 'background-color:rgb(' + red + ',' + green + ',' + blue + ');width:4px;height:4px;');
+    //     pixel.setAttribute('x', col);
+    //     pixel.setAttribute('y', row);
+
+    //     pixel.setAttribute('onclick', 'changeColor(this);');
+
+    //     let tableCol = document.createElement('td');
+
+    //     tableCol.append(pixel);
+
+
+    //     tableRow.append(tableCol);
+
+
+    //   }
+    //   table.append(tableRow);
+    // }
+
+    // did.append(table);
+    // document.body.append(did);
   });
 
 
@@ -121,11 +148,7 @@ document.addEventListener('DOMContentLoaded', () => {
     var changedPixel = document.querySelector('[x="' + data.x + '"][y="' + data.y + '"]');
     changedPixel.style.backgroundColor = 'rgb(' + data.color[0] + ', ' + data.color[1] + ', ' + data.color[2] + ')';
 
-    // $('[x="' + data.x + '"][y="' + data.y + '"]').css('background-color', 'rgb(' + data.color[0] + ', ' + data.color[1] + ', ' + data.color[2] + ')');
-    // changedPixel.css('background-color', 'rgb(' + data.color[0] + ', ' + data.color[1] + ', ' + data.color[2] + ')');
   });
-
-
 });
 
 function changeColor(element) {
@@ -140,24 +163,165 @@ function changeColor(element) {
 }
 
 function zoomIn() {
-  zoomLevel++;
+  // zoomLevel += 10;
+  zoomLevel *= 1.4142135624;
+
   zoom();
 }
 
 function zoomOut() {
-  zoomLevel--;
+  // zoomLevel -= 10;
+  zoomLevel /= 1.4142135624;
   zoom();
 }
 
 function zoom() {
-  var pixels = document.querySelectorAll('div');
-  console.log(pixels);
-  pixels.forEach(pixel => {
-    pixel.style.width = getZoomLevel();
-    pixel.style.height = getZoomLevel();
-  });
+  // var pixels = document.querySelectorAll('div');
+  // console.log(pixels);
+  // pixels.forEach(pixel => {
+  //   pixel.style.width = getZoomLevel();
+  //   pixel.style.height = getZoomLevel();
+  // });
+
+  canvas.style.width = getZoomLevel();
+  canvas.style.height = getZoomLevel();
 }
 
 function getZoomLevel() {
   return zoomLevel + 'px';
+}
+
+
+window.onload = function() {
+  var ctx = canvas.getContext('2d');
+
+  trackTransforms(ctx);
+
+  function redraw() {
+
+    // Clear the entire canvas
+    var p1 = ctx.transformedPoint(0, 0);
+    var p2 = ctx.transformedPoint(canvas.width, canvas.height);
+    ctx.clearRect(p1.x, p1.y, p2.x - p1.x, p2.y - p1.y);
+
+    ctx.save();
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.restore();
+
+    ctx.drawImage(image, 0, 0);
+
+  }
+  redraw();
+
+  var lastX = canvas.width / 2,
+    lastY = canvas.height / 2;
+
+  var dragStart, dragged;
+
+  canvas.addEventListener('mousedown', function(evt) {
+    document.body.style.mozUserSelect = document.body.style.webkitUserSelect = document.body.style.userSelect = 'none';
+    lastX = evt.offsetX || (evt.pageX - canvas.offsetLeft);
+    lastY = evt.offsetY || (evt.pageY - canvas.offsetTop);
+    dragStart = ctx.transformedPoint(lastX, lastY);
+    dragged = false;
+  }, false);
+
+  canvas.addEventListener('mousemove', function(evt) {
+    lastX = evt.offsetX || (evt.pageX - canvas.offsetLeft);
+    lastY = evt.offsetY || (evt.pageY - canvas.offsetTop);
+    dragged = true;
+    if (dragStart) {
+      var pt = ctx.transformedPoint(lastX, lastY);
+      ctx.translate(pt.x - dragStart.x, pt.y - dragStart.y);
+      redraw();
+    }
+  }, false);
+
+  canvas.addEventListener('mouseup', function(evt) {
+    dragStart = null;
+  }, false);
+
+  var scaleFactor = 1.1;
+
+  var zoom = function(clicks) {
+    var pt = ctx.transformedPoint(lastX, lastY);
+    ctx.translate(pt.x, pt.y);
+    var factor = Math.pow(scaleFactor, clicks);
+    ctx.scale(factor, factor);
+    ctx.translate(-pt.x, -pt.y);
+    redraw();
+  }
+
+  var handleScroll = function(evt) {
+    var delta = evt.wheelDelta ? evt.wheelDelta / 40 : evt.detail ? -evt.detail : 0;
+    if (delta) zoom(delta);
+    return evt.preventDefault() && false;
+  };
+
+  canvas.addEventListener('DOMMouseScroll', handleScroll, false);
+  canvas.addEventListener('mousewheel', handleScroll, false);
+};
+
+// https://codepen.io/techslides/pen/zowLd
+function trackTransforms(ctx){
+    var svg = document.createElementNS("http://www.w3.org/2000/svg",'svg');
+    var xform = svg.createSVGMatrix();
+    ctx.getTransform = function(){ return xform; };
+
+    var savedTransforms = [];
+    var save = ctx.save;
+    ctx.save = function(){
+        savedTransforms.push(xform.translate(0,0));
+        return save.call(ctx);
+    };
+
+    var restore = ctx.restore;
+    ctx.restore = function(){
+      xform = savedTransforms.pop();
+      return restore.call(ctx);
+	      };
+
+    var scale = ctx.scale;
+    ctx.scale = function(sx,sy){
+      xform = xform.scaleNonUniform(sx,sy);
+      return scale.call(ctx,sx,sy);
+	      };
+
+    var rotate = ctx.rotate;
+    ctx.rotate = function(radians){
+        xform = xform.rotate(radians*180/Math.PI);
+        return rotate.call(ctx,radians);
+    };
+
+    var translate = ctx.translate;
+    ctx.translate = function(dx,dy){
+        xform = xform.translate(dx,dy);
+        return translate.call(ctx,dx,dy);
+    };
+
+    var transform = ctx.transform;
+    ctx.transform = function(a,b,c,d,e,f){
+        var m2 = svg.createSVGMatrix();
+        m2.a=a; m2.b=b; m2.c=c; m2.d=d; m2.e=e; m2.f=f;
+        xform = xform.multiply(m2);
+        return transform.call(ctx,a,b,c,d,e,f);
+    };
+
+    var setTransform = ctx.setTransform;
+    ctx.setTransform = function(a,b,c,d,e,f){
+        xform.a = a;
+        xform.b = b;
+        xform.c = c;
+        xform.d = d;
+        xform.e = e;
+        xform.f = f;
+        return setTransform.call(ctx,a,b,c,d,e,f);
+    };
+
+    var pt  = svg.createSVGPoint();
+    ctx.transformedPoint = function(x,y){
+        pt.x=x; pt.y=y;
+        return pt.matrixTransform(xform.inverse());
+    }
 }
